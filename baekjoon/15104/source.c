22706c2fe363e5a3685a23227c43c481edf104e2
qilip <qilip@qilip.io>
2019-04-03 12:52:50 +0900

    fix 15104

--- baekjoon/15104/source.c
+++ baekjoon/15104/source.c
@@ -6,10 +6,10 @@ int main(void){
     scanf("%s", s);
     int len = strlen(s);
     int flg = 1;
-    for(int a=0;a<len-1;a++){
-        for(int b=a+1;b<len;b++){
+    for(int a=0;a<len;a++){
+        for(int b=a;b<len;b++){
             int ispal = 1;
-            for(int i=a, j=b;i<=b+1;i++,j--){
+            for(int i=a, j=b;i<=b;i++,j--){
                 if(s[i]!=s[j]){
                     ispal = 0;
                     break;
