1e7389fb38d5245c16181ea44b18f26c72b5fd42
qilip <qilip@qilip.io>
2019-04-06 18:38:40 +0900

    2019-04-06

---
+++ baekjoon/5419/source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        for(int i=0;i<n;i++){
+            int x, y;
+            scanf("%d %d", &x, &y);
+            
+        }
+    }
+    return 0;
+}
