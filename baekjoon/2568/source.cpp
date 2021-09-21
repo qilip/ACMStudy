263f0035a70de28f5bb3af7bff92b5e22b6800c5
qilip <qilip@qilip.io>
2021-09-21 06:25:24 +0000

    3년 방치후 격파

--- baekjoon/2568/source.cpp
+++ baekjoon/2568/source.cpp
@@ -10,28 +10,29 @@ int main(void){
     for(int i=0;i<n;i++){
         int ta, tb;
         scanf("%d %d", &ta, &tb);
-        b[tb] = ta;
-        a[ta] = 1;
+        b[ta] = tb;
     }
     for(int i=1;i<500001;i++){
         if(!b[i]) continue;
         if(lis[lptr-1]<b[i]){
+			a[i] = lptr;
             lis[lptr++] = b[i];
         }else{
-            int pt = lower_bound(lis+1, lis+lptr, b[i]) - (lis+1);
+            int pt = lower_bound(lis+1, lis+lptr, b[i]) - lis;
+			a[i] = pt;
             lis[pt] = b[i];
         }
     }
-    // TEST
-    for(int i=1;i<lptr;i++){
-        printf("%d ", lis[i]);
-    }
-    printf("\n");
-    // TEST
     printf("%d\n", n-lptr+1);
-    for(int i=1;i<lptr;i++){
-        a[lis[i]] = 0;
-    }
+    lptr--;
+	int ai = 500000;
+	while(lptr){
+		if(a[ai]==lptr){
+			a[ai] = 0;
+			lptr--;
+		}
+		ai--;
+	}
     for(int i=1;i<500001;i++){
         if(a[i]) printf("%d\n", i);
     }
