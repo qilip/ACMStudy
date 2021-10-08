4db12753b92d15fa8416c71013a3a6be858d621c
qilip <qilip@qilip.io>
2021-10-08 14:18:32 +0000

    solved.ac streak 25 with crypto

---
+++ baekjoon/10930/source.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const c = require('crypto');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const n = input[0];
+const cr = c.createHash('sha256').update(n).digest('hex');
+console.log(cr);
